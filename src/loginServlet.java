7ac3c0b4fa7e8f4e74f7134c105587fc8bab89ac
zxy2012 <617944292@qq.com>
2018-06-02 19:49:01 +0800

    Login by MySQL+JSP+Servlet.

--- src/loginServlet.java
+++ src/loginServlet.java
@@ -25,7 +25,6 @@ public class loginServlet extends HttpServlet {
         DBConn dbConn = new DBConn();
         String sql = "SELECT COUNT(1) FROM user WHERE username = ? AND password = ?";
         int count = dbConn.query(sql,new String[]{username,password});
-        System.out.println(count);
         if(count == 0){
             out.print("用户名或密码错误");
             out.print("<br><a href = 'index.jsp'>请重新登陆</a>");
