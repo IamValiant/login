ee7d7fd50d8df16a9bf6e0328afbd80bca3669e3
xueyongzeng <xueyongzeng@yangqianguan.com>
2018-11-16 19:36:21 +0800

    one

--- src/test.java
+++ src/test.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class test {
 	@Test
 	public void test(){
-		System.out.println("this is a test file for branch one!");
+		System.out.println("this is a test message for branch one!");
 		System.out.println("this is a test file for branch two!");
 	}
 
